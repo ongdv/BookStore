6453dc6031ccffc1351ce9e5d4b2ce610198cbd4
ongdv <ssokwoo@outlook.com>
2018-11-07 02:45:01 +0900

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Main from './Layout/Main.js';
+
+class App extends Component {
+  render() {
+    return (
+		<Fragment>
+			<Main />
+		</Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Layout/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Nav from '../Component/Nav';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+    			<Nav />            
+            </div>
+        );
+    }
+}
+
+export default Main;
